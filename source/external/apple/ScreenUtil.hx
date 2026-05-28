d2f01a12eaa151f217e6fde09edcf59273405dc8
SametGkTe <sametgkte@gmail.com>
2026-05-29 02:50:32 +0300

    belini kavrasam yeter

--- source/external/apple/ScreenUtil.hx
+++ source/external/apple/ScreenUtil.hx
@@ -1,14 +1,22 @@
 package external.apple;
 
 class ScreenUtil {
-	public static function getSafeAreaInsets(top:cpp.Pointer<Float>, bottom:cpp.Pointer<Float>, left:cpp.Pointer<Float>, right:cpp.Pointer<Float>):Void {
+	public static function getSafeAreaInsets(
+		top:cpp.RawPointer<Float>,
+		bottom:cpp.RawPointer<Float>,
+		left:cpp.RawPointer<Float>,
+		right:cpp.RawPointer<Float>
+	):Void {
 		if (top != null) top[0] = 0;
 		if (bottom != null) bottom[0] = 0;
 		if (left != null) left[0] = 0;
 		if (right != null) right[0] = 0;
 	}
 
-	public static function getScreenSize(width:cpp.Pointer<Float>, height:cpp.Pointer<Float>):Void {
+	public static function getScreenSize(
+		width:cpp.RawPointer<Float>,
+		height:cpp.RawPointer<Float>
+	):Void {
 		if (width != null) width[0] = 1280;
 		if (height != null) height[0] = 720;
 	}
